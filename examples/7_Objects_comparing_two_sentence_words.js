12a46b71a8a64706f3dc82c690035fad2229cb23
Venkata Chadalawada <venchad@gmail.com>
2019-03-17 13:17:48 -0500

    Update 7_Objects_comparing_two_sentence_words.js

--- examples/7_Objects_comparing_two_sentence_words.js
+++ examples/7_Objects_comparing_two_sentence_words.js
@@ -1,16 +1,48 @@
-// Complete the checkMagazine function below.
 function checkMagazine(magazine, note) {
-    var obj1 = {};
+    console.log('---magazine---', magazine);
+    console.log('---note---', note);
+    var noteObj = {}, magObj = {};
     if (magazine.length < note.length) {
-        return "NO";
+        return "No";
     }
     for (var i in note) {
-        obj1[note[i]] = 1;
+        (noteObj[note[i]]) ? noteObj[note[i]]
+            += 1 : noteObj[note[i]] = 1;
     }
     for (var j in magazine) {
-        if (obj1.hasOwnProperty(magazine[j])) {
-            delete obj1[magazine[j]];
+        (magObj[magazine[j]]) ? magObj[magazine[j]]
+            += 1 : magObj[magazine[j]] = 1;
+    }
+    console.log('---magObj---', magObj);
+    console.log('---noteObj---', noteObj);
+    for (var j in noteObj) {
+        // key in notes there is key in magazine
+        if (magObj[j]) {
+            noteObj[j]-=1;
+            magObj[j]-=1;
         }
     }
-    return (Object.keys(obj1).length === 0) ? 'Yes' : 'No';
+    console.log('---magObj---', magObj);
+    console.log('---noteObj---', noteObj);
+    for (var j in noteObj) {
+        if (noteObj[j] > 0) {
+            return "No"
+        }
+    }
+    return 'Yes';
 }
+
+//checkMagazine([ 'give', 'me', 'one', 'grand', 'today', 'night' ], [ 'give', 'one', 'grand', 'today' ]);
+/*
+X = "o l x imjaw bee khmla v o v o imjaw l khmla imjaw x";
+Y = "imjaw l khmla x imjaw o l l o khmla v bee o o imjaw imjaw o";
+checkMagazine(X1.split(" "), Y1.split(" "));
+
+
+X1 = "apgo clm w lxkvg mwz elo bg elo lxkvg elo apgo apgo w elo bg";
+Y1 = "elo lxkvg bg mwz clm w";
+
+
+X2 = "o l x imjaw bee khmla v o v o imjaw l khmla imjaw x";
+Y2 = "imjaw l khmla x imjaw o l l o khmla v bee o o imjaw imjaw o";
+*/
