1f4e63fe664ac0d5b421e587ec9231871baf7ec0
Venkata Chadalawada <venchad@gmail.com>
2019-04-07 17:15:33 -0500

    Create Permutate.js

---
+++ Permutate.js
@@ -0,0 +1,35 @@
+function getPerms(str){
+  function permutate(str){
+    console.log('=====', str);
+    if(str.length === 1){
+      return [str];
+    } else if(str.length === 2){
+      var arr = [];
+      arr.push(str);
+      arr.push(str[1]+str[0]);
+      return arr;
+    } else {
+      var permutations = [];
+      var size = str.length;
+      var lchar = str[size-1]; // p4
+      var remain = str.slice(0, size-1); //p1p2p3
+      // console.log('==',lchar, remain);
+      var word = permutate(remain); //p1p2p3 
+      // console.log('yo--', word);
+      for(var i=0;i<word.length;i++){
+        // console.log('**--',word[i]);
+        var eachword = word[i];
+        for(var j=0;j<eachword.length+1;j++){
+          var output = [word[i].slice(0, j), lchar, word[i].slice(j)].join('');
+          permutations.push(output);
+        }
+      }
+      return permutations;
+    }
+  }
+  
+  return permutate(str);
+}
+
+
+console.log('======================', getPerms('abcde'));
