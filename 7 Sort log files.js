b8e68d1503a64fac5b4990e37fb7afa1edb12095
Venkata Chadalawada <venchad@gmail.com>
2019-06-09 12:29:14 -0600

    Update 7 Sort log files.js

--- 7 Sort log files.js
+++ 7 Sort log files.js
@@ -8,15 +8,13 @@ function renderLogFiles(arr){
   let digits = logArrs.filter(l => numbers.includes(l[1][0]));
 
   letters = sortthem(letters);
-  digits = sortthem(digits);
-  
-  letters = letters.map(letter => letter[0]+' '+letter[1]);
-  digits = digits.map(digit => digit[0]+' '+digit[1]);
-  
+  // digits = sortthem(digits); // they want digits to be in same order
+ 
   // console.log('========= \n',letters);
   // console.log('--------- \n ',digits);
   return [
-    ...letters, ...digits
+    ...letters.map(l => l.join(' ')),
+    ...digits.map(l => l.join(' '))
     ];
 }
 
@@ -31,6 +29,32 @@ function sortthem(arr){
   });
 }
 
+var arr2 = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+console.log('------', renderLogFiles(arr2));
+
+/*
+var reorderLogFiles = function(logs) {
+  const logArrs = logs.map(l => l.split(' '));
+  const numbers = '1234567890'.split('');
+  let letters = logArrs.filter(l => !numbers.includes(l[1][0]));
+  const digits = logArrs.filter(l => numbers.includes(l[1][0]));
+  
+  letters = letters.map(l => { 
+      let [lead, ...others] = l;
+      return [lead, others.join(' ')];
+    })
+    .sort((a,b) => {
+      if(a[1] < b[1]) { return -1; }
+      if(a[1] >= b[1]) { return 1; } //keep head order
+      return 0;
+    });
+  
+  return [
+    ...letters.map(l => l.join(' ')),
+    ...digits.map(l => l.join(' '))
+  ];
+};
+
 var arr = [
  "a1 9 2 3 1",
  "g1 act car",
@@ -38,5 +62,6 @@ var arr = [
  "ab1 off key dog",
  "a8 act zoo"
 ];
-
-console.log('------', renderLogFiles(arr));
+var arr2 = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+console.log('------', reorderLogFiles(arr2));
+*/
