83d4300eaa7a11adba9ed13da4b890607809fa10
Venkata Chadalawada <venchad@gmail.com>
2019-06-10 18:42:17 -0500

    Create 10 Twtiiter design.js

---
+++ 10 Twtiiter design.js
@@ -0,0 +1,60 @@
+class Twitter{
+  
+  constructor() {
+    this.tweets = {};
+    this.follows = {};
+    this.time = 0;
+  }
+  
+  postTweet(userId, tweetId){
+    if(!this.tweets[userId]) { this.tweets[userId] = []; }
+    // both cases it should do push
+    this.tweets[userId].push([tweetId, this.time++]);
+  }
+  /* tweets = {
+      sai: [[123, 1],[125, 3],[126, 8], [132, 15]],
+      ramu: [[124, 2], [131, 13]] 
+     } */
+  
+  follows(followerId, followeeId){
+    if(!this.follows[followerId]) {
+      this.follows[followerId] = {};
+    }
+    this.follows[followerId][followeeId] = true;
+  }
+  /* follows = {
+      sai: {
+        ramu: true,
+        ravi: true,
+        },
+      ramu: {
+        somu: true,
+      } 
+     } */
+  
+  unfollows(followerId, followeeId){
+    if(this.follows[followerId]){
+      delete this.follows[followerId].followeeId;
+    }
+  }
+    
+  getNewsFeed(userId){
+    let feed = new Set();
+    // step1 - get own tweets
+    let timelyTweets = [... this.tweets[userId]]; // [[123, 1],[125, 3],[126, 8], [132, 15]]
+    let follows = this.follows[userId] || {};
+    // step2 - get all follwer tweets
+    for(let follow in follows){
+      timelyTweets.push( ...(this.tweets[follow] || []));
+    }
+    // step3 - sort them based on time they created for latest
+    timelyTweets.sort((a,b) => a[1]-b[1]);// sort based on 1 column which is time
+    //step4 - return top 10 for feed
+    for(let [id, time] of timelyTweets){
+      feed.add(id);
+      if(feed.size === 10) {break;}
+    }
+    
+    return [...feed];
+  }
+}
