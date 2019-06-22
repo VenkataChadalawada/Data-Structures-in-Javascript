144d8d53dee08cfcb6bad9e76522f10d7b1c3f03
Venkata Chadalawada <venchad@gmail.com>
2019-06-22 12:05:36 -0500

    Update 19Amazon Array_string.js

--- 19Amazon Array_string.js
+++ 19Amazon Array_string.js
@@ -3,7 +3,7 @@
 var twoSum = function(nums, target) {
     var obj = new Map();
     nums.map((val, ind) => {
-        obj.set(target-val, [val, ind]);
+        obj.set(target-val, [val, ind]); // SECRET
     });
     // console.log('---obj---', obj);
    
@@ -38,7 +38,22 @@ console.log(TwoSum([0,4,3,0],0))
 ---obj--- Map { 0 => [ 0, 3 ], -4 => [ 4, 1 ], -3 => [ 3, 2 ] }
 */
 
-// 2 lengthOfLongestSubstring
+// 2 Longest Substring Without Repeating Characters
+/*
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+             
+*/
 /**
  * @param {string} s
  * @return {number}
@@ -52,10 +67,51 @@ var lengthOfLongestSubstring = function(s) {
         if(seen[char]){
             start = Math.max(start, seen[char]);
         }
-        longest = Math.max(longest, i-start+1);
+        longest = Math.max(longest, i-start+1); // SECRET
         seen[char] = i+1;            
     }
     return longest;
 };
+
+
+// 3 atoi
+
+function atoi(str){
+   let index = 0, sign = 1, total = 0;
+    //1. Empty string
+    if(str.length === 0) return 0;
+
+    //2. Remove Spaces
+    while(str.charAt(index) == ' ' && index < str.length){
+        index ++;
+    }
+    //3. Handle signs
+    if(str.charAt(index) == '+' || str.charAt(index) == '-'){
+        sign = str.charAt(index) == '+' ? 1 : -1;
+        index ++;
+    }
+    
+    //4. Convert number and avoid overflow
+    while(index < str.length){
+        let digit = str.charAt(index) - '0'; // to get number from character
+        if(digit < 0 || digit > 9 || !digit){
+          break;
+        }
+        //check if total will be overflow after 10 times and add digit
+        if(Number.MAX_VALUE/10 < total || Number.MAX_VALUE/10 == total && Number.MAX_VALUE %10 < digit){
+            return sign == 1 ? Number.MAX_VALUE : Number.MIN_VALUE;
+        }
+
+        total = 10 * total + digit;
+        index ++;
+    }
+    return total * sign;
+}
+
+console.log(atoi("42"));
+console.log(atoi("   -42"));
+console.log(atoi("4193 with words"));
+console.log(atoi("words and 987"));
+console.log(atoi("-91283472332"));
 
 
