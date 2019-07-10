14c2ea929f9c2be638e39cea0aa1e80c033e64dd
Venkata Chadalawada <venchad@gmail.com>
2019-07-09 21:42:16 -0500

    Update 7 Sort log files.js

--- 7 Sort log files.js
+++ 7 Sort log files.js
@@ -1,11 +1,11 @@
 function renderLogFiles(arr){
   console.log(arr);
-  const logArrs = arr.map(l => l.split(' '));
+  const logArrs = arr.map(l => l.split(' ')); // [[g1, act, car],[zo4, 4, 7] ... ]
   // console.log(logArrs);
-  const numbers = '1234567890'.split('');
+  const numbers = '1234567890'.split('');  //[1,2,3,4,5,6,7,8,9,0]
   
-  let letters = logArrs.filter(l => !numbers.includes(l[1][0]));
-  let digits = logArrs.filter(l => numbers.includes(l[1][0]));
+  let letters = logArrs.filter(l => !numbers.includes(l[1][0])); // [g1, act, car] -> 'a' (true), [zo4, 4, 7] -> 4 (false), ... filters out those are not nums
+  let digits = logArrs.filter(l => numbers.includes(l[1][0])); // [g1, act, car] -> 'a' (false), [zo4, 4, 7] -> 4 (true), ... filters out those are not nums
 
   letters = sortthem(letters);
   // digits = sortthem(digits); // they want digits to be in same order
@@ -20,11 +20,15 @@ function renderLogFiles(arr){
 
 function sortthem(arr){
   return arr.map(l => {
-    let [lead, ...others] = l;
-    return [lead, others.join(' ')];
-  }).sort((a, b) => {
-    if(a[1] < b[1]) {return -1}
-    if(a[1] >= b[1]) {return 1;}
+    let [lead, ...others] = l; // great way of using rest operator it gets starting from second element into others
+    return [lead, others.join(' ')]; // 
+  }).sort((a, b) => { // a and b are current and next elements
+    if(a[1] < b[1]) {return -1;}
+    else if(a[1] >= b[1]) {return 1;}
+    // they might say if keys are same then leave it as it is
+    /* else if(a[0] === b[0]){
+      return 1;
+    } */
     return 0;
   });
 }
