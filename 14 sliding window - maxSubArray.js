ff52c90c43d17b76c048d9f3c253e1a11166a2fd
Venkata Chadalawada <venchad@gmail.com>
2020-02-08 17:50:31 -0700

    Update 14 sliding window - maxSubArray.js

--- 14 sliding window - maxSubArray.js
+++ 14 sliding window - maxSubArray.js
@@ -1,24 +1,8 @@
+// 1. maxSubArray Solution
+/*
+Given an array of integers and a number, write a function called maxSubarraySum , which finds the maximum sum of a subarray with the length of the number passed to the function. 
+*/
 function maxSubArraySum(arr, num){
-  let maxSum = 0; let tempSum = 0;
-  if(num>arr.length) return null;
-  for(let i=0;i<num;i++){
-    maxSum+=arr[i];
-  }
-  // console.log(sum);
-  tempSum = maxSum;
-  for(let j=num;j<arr.length;j++){
-    // each time substract jth position value & add j+num-1th position value
-    tempSum = tempSum+arr[j]-arr[j-num];
-    maxSum = Math.max(tempSum, maxSum);
-    //console.log(maxSum);
-  }
-  return maxSum;
-}
-
-console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 4));
-
-// maxSubArray Solution
-function maxSubarraySum(arr, num){
     if (arr.length < num) return null;
  
     let total = 0;
@@ -32,7 +16,32 @@ function maxSubarraySum(arr, num){
     }
     return total;
 }
-// minSubArrayLen Solution
+
+console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 4)); //22
+
+console.log(maxSubArraySum([100,200,300,400], 2)); //700
+
+console.log(maxSubArraySum([1,4,2,10,23,3,1,0,20], 4)); // 39
+
+console.log(maxSubArraySum([2,3], 3)); // null
+
+console.log(maxSubArraySum([3,-2,7,-4,1,-1,4,-2,1], 2)); // 5
+
+
+/*
+Write a function called minSubArrayLen  which accepts two parameters - an array of positive integers and a positive integer. 
+This function should return the minimal length of a contiguous subarray of which the sum is greater or equal to the integer passed to the function. If there isn't one, return 0 instead.
+*/
+// Example
+/*
+[2,3,1,1,-1,6,4,3,8], k=7
+ 2,3,1,1 = 7
+     1,1,-1,6 = 7
+              4,3 = 7
+
+*/
+
+// 2. minSubArrayLen Solution
 function minSubArrayLen(nums, sum) {
   let total = 0;
   let start = 0;
@@ -61,7 +70,22 @@ function minSubArrayLen(nums, sum) {
  
   return minLen === Infinity ? 0 : minLen;
 }
-// findLongestSubstring Solution
+/*
+console.log(minSubArrayLen([2,3,1,2,4,3], 7)); // 2 -> [4,3] is the smallest subarray
+
+console.log(minSubArrayLen([2,1,6,5,4], 9)); // 2 -> [5,4]
+
+console.log(minSubArrayLen([3,1,7,11,2,9,8,21, 62,33,19], 52)); // 1 [62] > 52
+
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10], 39)); // 3
+
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10], 55)); // 5
+
+console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10], 95)); // 5
+*/
+
+
+// 3. findLongestSubstring Solution
 function findLongestSubstring(str) {
   let longest = 0;
   let seen = {};
