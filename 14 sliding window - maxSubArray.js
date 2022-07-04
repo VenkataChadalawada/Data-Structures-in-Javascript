82b5f24315f72619605c1642dd6c1aaeeac933cd
Venkata Chadalawada <venchad@gmail.com>
2022-07-04 16:08:46 -0500

    Update 14 sliding window - maxSubArray.js

--- 14 sliding window - maxSubArray.js
+++ 14 sliding window - maxSubArray.js
@@ -85,7 +85,21 @@ console.log(minSubArrayLen([1,4,16,22,5,7,8,9,10], 95)); // 5
 */
 
 
-// 3. findLongestSubstring Solution
+
+//  findLongestSubstring Solution -  Longest Substring Without Repeating Characters
+/*
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Input: "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3. 
+             Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+             
+*/
 function findLongestSubstring(str) {
   let longest = 0;
   let seen = {};
